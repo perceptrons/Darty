7d7c125b088ad9596cd9e5281987e246767d5bd0
Adam Benabbou <russweas@gmail.com>
2019-02-02 22:26:19 -0600

    Just added a pic of brandon

---
+++ test_image.py
@@ -0,0 +1,17 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2 as cv
+
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+#allow camera to warm up.
+time.sleep(0.1)
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+
+#display the image on screen and wait for a keypress
+cv.imshow("Image.png", image)
+cv.waitKey(0)
+
