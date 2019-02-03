57f3452d383a3f0537a65b032230dac02909ec26
Brandon Lucas <thebrandonlucas@gmail.com>
2019-02-03 08:59:15 -0600

    darty board is fixed! We can almost play now!

--- site/darty.js
+++ site/darty.js
@@ -1,88 +1,101 @@
 $(document).ready(function() {
   $.ajaxSetup({cache: false});
-  map = "dartboard";
-  coordsList = ['100,100 100,400 400,400 400,100 100,100', '200,10 250,190 160,210',
-                '0,0 0,50 50,50 50,0 0,0'];
-  createSVG(window.location.href, 100, 100);
-  var svgNS = document.getElementsByTagName("svg")[0].namespaceURI;
-  populateSVG(coordsList, map, svgNS);
-  zeroFillRegionData();
 
-  // Setup the game
-  $('.start-btn').click(function() {
-    $('.game-board-setup').removeClass('hidden');
-    $('.start-btn').addClass('hidden');
-  });
+  // coordsList = ['100,100 100,400 400,400 400,100 100,100', '200,10 250,190 160,210',
+  //             '0,0 0,50 50,50 50,0 0,0'];
 
-  // Show clicked region
-  $('.poly').click(function() {
-    $this = $(this);
-    // make sure everything is unselected
-    $('.poly').removeClass('poly-selected');
-    $('.poly').addClass('poly-unset');
-    $this.addClass('poly-selected');
-    $this.removeClass('poly-unset');
-    // TODO: make poly-set z-index lower priority than selected index but don't remove it
-  });
+  $.getJSON('regions.json', function(coordsList) {
+      createSVG(window.location.href, 100, 100);
+    // console.log("asdf" + svgNS);
+    var svgNS = document.getElementsByTagName("svg")[0].namespaceURI;
+    map = "dartboard";
+    var len = Object.keys(coordsList).length;
+    // coordsList = ['100,100 100,400 400,400 400,100 100,100', '200,10 250,190 160,210',
+    //             '0,0 0,50 50,50 50,0 0,0'];
+    coordsList.length = len;
+    coordsList = Array.prototype.slice.call(coordsList);
+    populateSVG(coordsList, map, svgNS);
 
-  // Submit region file form
-  $('.region-points-btn').click(function() {
-    // check that a poly is clicked
-    if ($('.poly-selected').attr("name") == undefined) {
-      alert("Please click a region to enter a value!");
-      return;
-    }
-    var regionName = $('.poly-selected').attr("name");
-    var regionPoints = $('.region-points-input').val();
-    $("[name='" + regionName + "']").attr("value", regionPoints);
-    $('[name="hover-region-value"]').empty();
-    $('[name="hover-region-value"]').append($(".poly-selected").attr("value"));
-    if (regionPoints == "" || isNaN(regionPoints) == true) {
-      alert("Please enter a number for this region!");
-    } else {
-      $.post('writeRegionPointsToFile.php', {regionName: regionName, regionPoints: regionPoints},
-      function(result) {
-        console.log(regionPoints + " written to region_data.json!");
-      });
-      $('.poly-selected').addClass('poly-set');
-      checkGameReady();
-    }
-  });
+    // populateSVG(coordsList, map, svgNS);
+    zeroFillRegionData();
 
-  // Display point value of currently hovered-on poly
-  $('.poly').hover(function() {
-      $('[name="hover-region-value"]').empty();
-      $('[name="hover-region-value"]').append($(this).attr("value"));
-  });
-
-  // PLAY GAME
-  var player_1_score = 0;
-  var player_2_score = 0;
-  $('[name="play-game-btn"]').click(function() {
-    $(".game-board-setup").addClass("hidden");
-    $('[name="player-2-div"]').addClass("hidden");
-    $(".game-board-active").removeClass("hidden");
-  });
-
-  // Play/pause functionality - make sure to write to file!
-  var scoreMonitoring = "True";
-  $('[name="play-btn"]').click(function() {
-    $('[name="pause-btn"]').removeClass('hidden');
-    $('[name="play-btn"]').addClass('hidden');
-    scoreMonitoring = "False";
-    $.post('scoreMonitoring.php', {scoreMonitoring: scoreMonitoring}, function(result) {
-      console.log("score monitoring: " + scoreMonitoring);
+    // Setup the game
+    $('.start-btn').click(function() {
+      $('.game-board-setup').removeClass('hidden');
+      $('.start-btn').addClass('hidden');
     });
-  });
-  $('[name="pause-btn"]').click(function() {
-    $('[name="play-btn"]').removeClass('hidden');
-    $('[name="pause-btn"]').addClass('hidden');
-    scoreMonitoring = "True";
-    $.post('scoreMonitoring.php', {scoreMonitoring: scoreMonitoring}, function(result) {
-      console.log("score monitoring: " + scoreMonitoring);
+
+    // Show clicked region
+    $('.poly').click(function() {
+      $this = $(this);
+      // make sure everything is unselected
+      $('.poly').removeClass('poly-selected');
+      $('.poly').addClass('poly-unset');
+      $this.addClass('poly-selected');
+      $this.removeClass('poly-unset');
+      // TODO: make poly-set z-index lower priority than selected index but don't remove it
+    });
+
+    // Submit region file form
+    $('.region-points-btn').click(function() {
+      // check that a poly is clicked
+      if ($('.poly-selected').attr("name") == undefined) {
+        alert("Please click a region to enter a value!");
+        return;
+      }
+      var regionName = $('.poly-selected').attr("name");
+      var regionPoints = $('.region-points-input').val();
+      $("[name='" + regionName + "']").attr("value", regionPoints);
+      $('[name="hover-region-value"]').empty();
+      $('[name="hover-region-value"]').append($(".poly-selected").attr("value"));
+      if (regionPoints == "" || isNaN(regionPoints) == true) {
+        alert("Please enter a number for this region!");
+      } else {
+        $.post('writeRegionPointsToFile.php', {regionName: regionName, regionPoints: regionPoints},
+        function(result) {
+          console.log(regionPoints + " written to region_data.json!");
+        });
+        $('.poly-selected').addClass('poly-set');
+        checkGameReady();
+      }
+    });
+
+    // Display point value of currently hovered-on poly
+    $('.poly').hover(function() {
+        $('[name="hover-region-value"]').empty();
+        $('[name="hover-region-value"]').append($(this).attr("value"));
+    });
+
+    // PLAY GAME
+    var player_1_score = 0;
+    var player_2_score = 0;
+    $('[name="play-game-btn"]').click(function() {
+      $(".game-board-setup").addClass("hidden");
+      $('[name="player-2-div"]').addClass("hidden");
+      $(".game-board-active").removeClass("hidden");
+    });
+
+    // Play/pause functionality - make sure to write to file!
+    var scoreMonitoring = "True";
+    $('[name="play-btn"]').click(function() {
+      $('[name="pause-btn"]').removeClass('hidden');
+      $('[name="play-btn"]').addClass('hidden');
+      scoreMonitoring = "False";
+      $.post('scoreMonitoring.php', {scoreMonitoring: scoreMonitoring}, function(result) {
+        console.log("score monitoring: " + scoreMonitoring);
+      });
+    });
+    $('[name="pause-btn"]').click(function() {
+      $('[name="play-btn"]').removeClass('hidden');
+      $('[name="pause-btn"]').addClass('hidden');
+      scoreMonitoring = "True";
+      $.post('scoreMonitoring.php', {scoreMonitoring: scoreMonitoring}, function(result) {
+        console.log("score monitoring: " + scoreMonitoring);
+      });
     });
   });
 });
+
 
 // create the svg from a list of coordinates
 function populateSVG(coordsList, id, svgNS) {
@@ -114,6 +127,7 @@ function zeroFillRegionData() {
     poly_names.push($(polys[i]).attr("name"));
     zeros.push("0");
   }
+  console.log(poly_names);
   $.post('zeroFillRegionData.php', {regionNames: poly_names, regionPoints: zeros},
   function(result) {
     console.log("Zeros written to region_data.json!");
@@ -124,4 +138,12 @@ function zeroFillRegionData() {
 function checkGameReady() {
   if ($("polygon").length == $(".poly-set").length)
     $('[name="play-game-btn"]').removeClass("hidden");
+}
+
+function getRegionsData(coordsList, map, svgNS) {
+  this.coordsList = null;
+  $.getJSON('regions.json', function(json) {
+    this.coordsList = json;
+    populateSVG(coordsList, map, svgNS);
+  }.bind(this));
 }
